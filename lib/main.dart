4196d5b34d7064ba3f3c732202b87487c47f9742
Boris <edemwomagno@gmail.com>
2026-08-17 16:40:02 +0000

    Fix Flutter adding device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:application/screens/home/home_screen.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'constants.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: kIsWeb,
+      builder: (context) => const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -14,6 +21,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       theme: ThemeData(
         textTheme: Theme.of(
           context,
